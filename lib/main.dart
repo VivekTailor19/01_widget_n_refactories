d75abafde485a42f31bf963f68ee3e94f7d268b9
Vivek <tailorvivekgec19@gmail.com>
2023-02-17 22:56:53 -0800

    RNW

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:lecture1/01_audi_logo/audi_logo.dart';
 import 'package:lecture1/02%20Mission%20of%20RNW/mission_rnw.dart';
 import 'package:lecture1/03%20Mix-Up/mix_up.dart';
 import 'package:lecture1/04%20Mashal/mashal.dart';
+import 'package:lecture1/08_Emoji/emoji.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Mission_RNW(),
+      home: Emoji(),
     ),
   );
 }
